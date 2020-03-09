2df0b618ccfc975acf20433fb9167d748f874667
Levy Shi <levyshi@buffalo.edu>
2020-03-09 18:30:02 -0400

    changed print statements to logs

--- basic_trader/model/info_stream.py
+++ basic_trader/model/info_stream.py
@@ -1,21 +1,22 @@
+import logging
 from alpaca_trade_api import StreamConn
 conn = StreamConn()
 
 @conn.on(r'^account_updates$')
 async def on_account_updates(conn, channel, account):
-    print('account', account)
+    logging.debug('account', account)
 
 @conn.on(r'^trade_updates$')
 async def on_status(conn, channel, data):
-    print('trade updates', data)
+    logging.debug('trade updates', data)
 
 @conn.on(r'^AM$')
 async def on_minute_bars(conn, channel, bar):
-    print('bars', bar)
+    logging.debug('bars', bar)
 
 @conn.on(r'^A$')
 async def on_second_bars(conn, channel, bar):
-    print('bars', bar)
+    logging.debug('bars', bar)
 
 # blocks forever
 conn.run(['account_updates', 'A.*', 'trade_updates'])
